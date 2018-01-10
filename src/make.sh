ae72abcbd6af912afb575a0b53999f0a52764cac
Alan Ansell <aans768@aucklanduni.ac.nz>
2018-01-11 09:47:50 +1300

    increase number of AMAF records allocated

--- src/make.sh
+++ src/make.sh
@@ -1,1 +1,1 @@
-g++ -std=c++11 -D UCB_C=0.1 -D RAVE_C=1e-3 -D SLOW_ -D DEBUG_ -Wall -O2 -o ../../bin/EventHorizon4_0_1 main.cpp Position.cpp globals.cpp MCTS.cpp AMAF.cpp
+g++ -std=c++11 -D UCB_C=0.1 -D RAVE_C=1e-3 -D FAST_ -D DEBUG_ -Wall -O2 -o ../../bin/EventHorizon4_0_1 main.cpp Position.cpp globals.cpp MCTS.cpp AMAF.cpp

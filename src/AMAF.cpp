945f98f79a6b211dae92bbfc2f4276817cfa0fd6
Alan Ansell <aans768@aucklanduni.ac.nz>
2018-01-31 15:58:26 +1300

    Let siblings share AMAF statistics

--- src/AMAF.cpp
+++ src/AMAF.cpp
@@ -22,7 +22,7 @@ void AMAFTable::init(U32 n_open, U32 n_stones) {
     n_stones_ = n_stones;
     n_open_ = n_open;
     amaf_.clear(); 
-    heap_ = std::vector<AMAFRecord*>();
+    list_.clear();
 }
 
 
@@ -36,21 +36,6 @@ AMAFRecord * AMAFTable::cell_init(U32 cell_index) {
     for (U32 i = 0; i < n_stones_; i++)
         records[i].init_generated();
     return records;
-}
-
-
-void AMAFTable::play(U32 cell_index, U32 stone_index) {
-    check_initialised();
-    AMAFRecord * records = amaf_[cell_index];
-    if (records == NULL)
-        records = cell_init(cell_index);
-    AMAFRecord& record = records[stone_index];
-#ifdef DEBUG_
-    assert(! record.played_);
-#endif
-    if (record.n_playouts_ != 0)
-        pop_heap();
-    record.played_ = true;
 }
 
 
@@ -82,21 +67,18 @@ void AMAFTable::update(U32 cell_index, U32 stone_index, bool win) {
     AMAFRecord& record = records[stone_index];
     if (record.n_playouts_ == 0) {
         record.init(cell_index, stone_index, win);
-        if (! record.played_)
-            heap_insert(&record);
+        list_.push_back(record);
     } else {
         record.n_wins_ += win;
         record.n_playouts_++;
 #ifdef DEBUG_
         assert(record.n_playouts_ < 60000);
 #endif
-        if (! record.played_)
-            heap_update(&record, win);
     }
 }
 
 
-void AMAFTable::get_best(U32& cell_index, U32& stone_index) {
+/*void AMAFTable::get_best(U32& cell_index, U32& stone_index) {
     if (heap_.empty()) {
         cell_index = NO_CELL;
         return;
@@ -106,125 +88,6 @@ void AMAFTable::get_best(U32& cell_index, U32& stone_index) {
     stone_index = record->stone_index_;
     //record->tried_ = true;
     //pop_heap();
-}
+}*/
 
-
-bool AMAFTable::is_played(U32 cell_index, U32 stone_index) {
-    if (amaf_.size() == 0)
-        return false;
-    AMAFRecord * records = amaf_[cell_index];
-    if (records == NULL)
-        return false; 
-    return records[stone_index].played_;
-}
-
-
-void AMAFTable::heap_insert(AMAFRecord * record) {
-    U32 index = heap_.size();
-    heap_.push_back(NULL);
-    while (index) {
-        U32 parent = (index - 1) >> 1;
-        AMAFRecord * rep = heap_[parent];
-        if (*record < *rep)
-            break;
-        rep->index_ = index;
-        heap_[index] = rep;
-        index = parent;
-    }
-    record->index_ = index;
-    heap_[index] = record;
-}
-
-
-void AMAFTable::pop_heap() {
-#ifdef DEBUG_
-    assert(heap_.size() > 0);
-#endif
-    //printf("In pop_heap\n");
-    //fflush(stdout);
-    AMAFRecord * record = heap_.back();
-    heap_.pop_back();
-    U32 size = heap_.size();
-    U32 first_leaf = (size >> 1);
-    U32 index = 0;
-    while (index < first_leaf) {
-        U32 left = (index << 1) + 1;
-        U32 right = left + 1;
-        AMAFRecord * child;
-        if (right < size && *heap_[left] < *heap_[right]) {
-            child = heap_[right];
-            if (*record < *child) {
-                child->index_ = index;
-                heap_[index] = child;
-                index = right;
-            } else {
-                break;
-            }
-        } else {
-            child = heap_[left];
-            if (*record < *child) {
-                child->index_ = index;
-                heap_[index] = child;
-                index = left;
-            } else {
-                break;
-            }
-        }
-    }
-    record->index_ = index;
-    heap_[index] = record;
-}
-
-
-void AMAFTable::heap_update(AMAFRecord * record, bool win) {
-    //printf("In heap_update\n");
-    //fflush(stdout);
-    U32 index = record->index_;
-    if (win) {
-        //printf("Win\n");
-        //fflush(stdout);
-        while (index) {
-            U32 parent = (index - 1) >> 1;
-            AMAFRecord * rep = heap_[parent];
-            if (*record < *rep)
-                break;
-            rep->index_ = index;
-            heap_[index] = rep;
-            index = parent;
-        }
-        record->index_ = index;
-        heap_[index] = record;
-    } else {
-        //printf("Loss\n");
-        //fflush(stdout);
-        U32 size = heap_.size();
-        U32 first_leaf = (size >> 1);
-        while (index < first_leaf) {
-            U32 left = (index << 1) + 1;
-            U32 right = left + 1;
-            AMAFRecord * child;
-            if (right < size && *heap_[left] < *heap_[right]) {
-                child = heap_[right];
-                if (*record < *child) {
-                    child->index_ = index;
-                    heap_[index] = child;
-                    index = right;
-                } else {
-                    break;
-                }
-            } else {
-                child = heap_[left];
-                if (*record < *child) {
-                    child->index_ = index;
-                    heap_[index] = child;
-                    index = left;
-                } else {
-                    break;
-                }
-            }
-        }
-        record->index_ = index;
-        heap_[index] = record;
-    }
-}
 
--- src/AMAF.h
+++ src/AMAF.h
@@ -10,14 +10,11 @@
 struct AMAFRecord {
     U16 n_wins_;
     U16 n_playouts_;
-    U16 index_;
     U8 cell_index_;
     U8 stone_index_;
-    bool played_;
-    
+
     inline void init_generated() {
         n_playouts_ = 0;
-        played_ = false;
     }
      
     void init(U32 cell_index, U32 stone_index, bool win) {
@@ -25,10 +22,7 @@ struct AMAFRecord {
         stone_index_ = (U8)stone_index;
         n_wins_= (U16)win;
         n_playouts_ = 1;
-    }
-
-    inline bool operator < (const AMAFRecord& other) const {
-        return (U64)n_wins_ * (U64)other.n_playouts_ < (U64)other.n_wins_ * (U64)n_playouts_;
+        next_ = NULL;
     }
 
 };
@@ -48,9 +42,9 @@ class AMAFTable {
 public:
     U32 n_stones_;
     U32 n_open_;
-    std::vector<AMAFRecord*> heap_;
+    std::vector<AMAFRecord*> list_;
     std::vector<AMAFRecord*> amaf_;
-    
+     
     void init(U32 n_open, U32 n_stones);
    
     AMAFRecord * cell_init(U32 cell_index);
@@ -60,24 +54,13 @@ public:
             amaf_ = std::vector<AMAFRecord*>(n_open_, NULL);
     }
      
-    void play(U32 cell_index, U32 stone_index);
-    
     U32 get_n_playouts(U32 cell_index, U32 stone_index);
     
     Real get_value(U32 cell_index, U32 stone_index);
 
     void update(U32 cell_index, U32 stone_index, bool win); 
 
-    void get_best(U32& cell_index, U32& stone_index);
-
-    bool is_played(U32 cell_index, U32 stone_index);
-
-private:
-    void heap_insert(AMAFRecord * record);
-
-    void pop_heap();
-
-    void heap_update(AMAFRecord * record, bool win);
+    void get_best(U32& cell_index, U32& stone_index, const std::vector<U32>& tried) const;
 };
 
 #endif
